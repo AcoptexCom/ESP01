283aec9739d96474ae8d039edcd0099921fc0b83
AcoptexCom <58400137+AcoptexCom@users.noreply.github.com>
2020-04-16 21:38:48 +0300

    Add files via upload

--- ESP8266_HILINK/ESP8266_HILINK.ino
+++ ESP8266_HILINK/ESP8266_HILINK.ino
@@ -32,17 +32,16 @@ void setup() {
   digitalWrite(output2, LOW);
 
   // Connect to Wi-Fi network with SSID and password
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
-  WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
-  // Print local IP address and start web server
-  Serial.println("");
-  Serial.println("WiFi connected.");
-  Serial.println("IP address: ");
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  
+  // Print local IP address and start web server   
+  Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
   server.begin();
 }
